e06f079bd1bad8660ae7442555b703e340eec958
VEPallavi <Pallavisinha@virtualemployee.com>
2021-01-21 22:59:23 +0530

    done

--- app/src/main/java/com/ve/lawyer/activity/ChatActivity.java
+++ app/src/main/java/com/ve/lawyer/activity/ChatActivity.java
@@ -1,18 +1,39 @@
 package com.ve.lawyer.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ve.lawyer.R;
+import com.ve.lawyer.adapter.ChatAdapter;
+import com.ve.lawyer.model.ChatModel;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatActivity extends AppCompatActivity {
+    private ChatAdapter ca;
+    private ArrayList<ChatModel> listChat;
+    private RecyclerView rv_chat;
+    private ImageView iv_smily;
+    private EditText et_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        rv_chat = findViewById(R.id.rv_chat);
+        iv_smily = findViewById(R.id.iv_smily);
+        et_message = findViewById(R.id.et_message);
+        rv_chat.setLayoutManager(new LinearLayoutManager(this));
         clicks();
+        dummy();
     }
 
     void clicks() {
@@ -20,5 +41,53 @@ public class ChatActivity extends AppCompatActivity {
             finish();
 
         });
+        iv_smily.setOnClickListener((v) -> {
+            if (et_message.getText().toString().trim().length() > 0) {
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("hh.mm aa");
+                ChatModel cm = new ChatModel();
+                cm.setFromId("1");
+                cm.setMessage(et_message.getText().toString().trim());
+                cm.setName("Rahul");
+                cm.setTime(dateFormat.format(new Date()));
+                listChat.add(cm);
+                ca.notifyDataSetChanged();
+                rv_chat.smoothScrollToPosition(listChat.size());
+                et_message.setText("");
+
+            } else {
+                Toast.makeText(this, "Please enter text", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+
+    void dummy() {
+        listChat = new ArrayList<>();
+
+
+        ChatModel cm;
+
+
+        cm = new ChatModel();
+        cm.setFromId("2");
+        cm.setMessage("Hi");
+        cm.setName("User");
+        cm.setTime("9:30 AM");
+        listChat.add(cm);
+
+        cm = new ChatModel();
+        cm.setFromId("1");
+        cm.setMessage("Hello, How are you");
+        cm.setName("User");
+        cm.setTime("9:31 AM");
+        listChat.add(cm);
+
+
+        ca = new ChatAdapter(this, listChat);
+        rv_chat.setAdapter(ca);
+
+
     }
 }
---
+++ app/src/main/java/com/ve/lawyer/model/ChatModel.java
@@ -0,0 +1,42 @@
+package com.ve.lawyer.model;
+
+public class ChatModel {
+
+
+    private String fromId;
+    private  String message;
+    private String Name;
+    private String time;
+
+    public String getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
